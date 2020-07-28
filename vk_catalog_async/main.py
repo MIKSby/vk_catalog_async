09441196aba4f581f024f1aeb9d4334a03a84b9f
Mikhail Klimovich <mikhail12klimovich@gmail.com>
2020-07-28 21:05:26 +0300

    create async storing to file functionality

--- vk_catalog_async/main.py
+++ vk_catalog_async/main.py
@@ -3,6 +3,7 @@ import random
 from typing import NamedTuple, Optional, Dict, Any
 
 import httpx
+from aiofile import AIOFile
 from bs4 import BeautifulSoup
 from httpcore import ConnectTimeout
 
@@ -17,6 +18,7 @@ class SectionLink(NamedTuple):
 
 QUEUE__SECTION = 'queue__section_links'
 QUEUE__WEB = 'queue__web'
+QUEUE__STORING = 'queue__storing'
 
 
 async def get_links_from_catalog_by_class_name(all_queues: Dict[str, Any]) -> None:
@@ -29,8 +31,8 @@ async def get_links_from_catalog_by_class_name(all_queues: Dict[str, Any]) -> No
                     continue
                 try:
                     if section_link.nesting_lvl == 3:
-                        print(f'https://vk.com/{tag["href"]}')
-                        return
+                        vk_id = tag["href"][2:]
+                        await all_queues[QUEUE__STORING].put(vk_id)
                     else:
                         new_section_link = SectionLink(url=f'https://vk.com/{tag["href"]}', nesting_lvl=section_link.nesting_lvl + 1, last_response=None)
                         await all_queues[QUEUE__WEB].put(new_section_link)
@@ -53,22 +55,38 @@ async def fetch(all_queues: Dict[str, Any], user_agent: str) -> None:
         print(f'web task complete! in web queue: {all_queues[QUEUE__WEB].qsize()}; in sector queue: {all_queues[QUEUE__SECTION].qsize()} tasks')
 
 
-async def main() -> None:
+async def write_to_file(all_queues: Dict[str, Any]) -> None:
+    while True:
+        id_vk = await all_queues[QUEUE__STORING].get()
+        async with AIOFile('result.txt', 'a') as f:
+            await f.write(f'{id_vk}\n')
+
+
+async def main(local_scan_workers_count: int, web_workers_count: int, writers_count: int) -> None:
     queue__section_links: Any = asyncio.Queue()
     queue__web: Any = asyncio.Queue()
+    queue__storing: Any = asyncio.Queue()
 
     all_queues = {
         QUEUE__SECTION: queue__section_links,
         QUEUE__WEB: queue__web,
+        QUEUE__STORING: queue__storing,
     }
 
     await queue__web.put(SectionLink(url='https://vk.com/catalog.php', nesting_lvl=0, last_response=None))
 
-    local_scan_worker = asyncio.create_task(get_links_from_catalog_by_class_name(all_queues))
-    web_workers = [asyncio.create_task(fetch(all_queues, user_agent=random.choice(USER_AGENTS))) for _ in range(300)]
+    local_scan_workers = [asyncio.create_task(get_links_from_catalog_by_class_name(all_queues)) for _ in range(local_scan_workers_count)]
+    web_workers = [asyncio.create_task(fetch(all_queues, user_agent=random.choice(USER_AGENTS))) for _ in range(web_workers_count)]
+    writers = [asyncio.create_task(write_to_file(all_queues)) for _ in range(writers_count)]
 
-    await asyncio.gather(local_scan_worker, *web_workers)
+    await asyncio.gather(*local_scan_workers, *web_workers, *writers)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    asyncio.run(
+        main(
+            local_scan_workers_count=1,
+            web_workers_count=10,
+            writers_count=40,
+        )
+    )
